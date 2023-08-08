c7cce93f398cb78818f4a8b0bea417167208c4a0
TEO <alfredo.ssm@gmail.com>
2023-08-07 20:29:53 -0400

    fix: img folder name

--- src/app/pages/login-and-register/login-and-register.component.spec.ts
+++ src/app/pages/login-and-register/login-and-register.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { LoginAndRegisterComponent } from './login-and-register.component';
+import { LoginAndRegisterComponent } from './login-and-register.component'
 
 describe('LoginAndRegisterComponent', () => {
-  let component: LoginAndRegisterComponent;
-  let fixture: ComponentFixture<LoginAndRegisterComponent>;
+  let component: LoginAndRegisterComponent
+  let fixture: ComponentFixture<LoginAndRegisterComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginAndRegisterComponent]
-    });
-    fixture = TestBed.createComponent(LoginAndRegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      declarations: [LoginAndRegisterComponent],
+    })
+    fixture = TestBed.createComponent(LoginAndRegisterComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
