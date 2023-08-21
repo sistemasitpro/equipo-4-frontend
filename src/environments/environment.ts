183abce66c008accd47d1e3298f5b3b756dfd16f
sistemasitpro <sistemasitpro@proton.me>
2023-08-21 12:58:00 +0200

    modify url base for stage environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 // environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://pre.api-nest.careyourspets.duckdns.org',
+  apiUrl: 'https://pre.api.lcareyourspets.duckdns.org/nestjs',
 }
